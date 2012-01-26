71efdec4c9a92de3a6097122ac57e6f368a5737e
scrogster <scrogster@gmail.com>
2012-01-26 22:30:24 +1100

    minor doc revision and enlargement

--- R/chapman.r
+++ R/chapman.r
@@ -1,21 +1,3 @@
-#' Chapman Robson catch curve survival estimator
-#' 
-#' Computes the Chapman-Robson survival estimator and it's standard error from
-#' age structure data
-#' 
-#' 
-#' @param ages vector of individual ages, coded such that the first
-#' fully-recruited age class is coded as zero.
-#' @return list with elemets S_hat and SE(S_hat)
-#' @author Michael Scroggie
-#' @examples
-#' 
-#' data(geocrinia)
-#' chapman(geocrinia$age)
-#' 
-#' randata<-rgeom(1000, 1-0.3)
-#' chapman(randata)
-#' 
 chapman<-function(ages){
   X<-sum(ages)
   n<-length(ages)
