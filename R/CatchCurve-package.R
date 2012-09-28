a98a4996816d308f84fec2a9b49670839e84248e
scrogster <scrogster@gmail.com>
2012-09-28 23:27:23 +1000

    oxygenized documentation using Rd2roxygen

---
+++ R/CatchCurve-package.R
@@ -0,0 +1,20 @@
+
+
+#'Age data from populations of \emph{Geocrinia laevis}, \emph{G. victoriana}
+#'and their hybrids
+#'
+#'Includes ages, sexes, populations and whether hybrid or not
+#'
+#'
+#'@name geocrinia
+#'@docType data
+#'@format A dataframe containing ages of frogs (determined using
+#'skeletochronology), along with covariates for sex, population of origin and
+#'whether hybrid or not
+#'@source Scroggie, M.P. (2012) Survival of adult smooth froglets
+#'(\emph{Geocrinia laevis} complex, Anura, Myobatrachidae) in and around a
+#'hybrid zone. \emph{Herpetological Conservation and Biology} 152: 136-144.
+NULL
+
+
+
--- R/chapman.r
+++ R/chapman.r
@@ -1,3 +1,22 @@
+#'Chapman Robson catch curve survival estimator
+#'
+#'Computes the Chapman-Robson survival estimator and it's standard error from
+#'age structure data
+#'
+#'
+#'@param ages vector of individual ages, coded such that the first
+#'fully-recruited age class is zero.
+#'@return list with elements S_hat and SE(S_hat)
+#'@author Michael Scroggie
+#'@references Chapman, D.G. and Robson, DS (1960) The analysis of a catch
+#'curve. \emph{Biometrics} 16:354-368.
+#'@examples
+#'
+#'data(geocrinia)
+#'chapman(geocrinia$age)
+#'randata<-rgeom(1000, 1-0.3)
+#'chapman(randata)
+#'
 chapman<-function(ages){
   X<-sum(ages)
   n<-length(ages)
